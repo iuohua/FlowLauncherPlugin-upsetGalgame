4085095a6018c428917334f4e0555b7745879909
luohua <156412791+iuohua@users.noreply.github.com>
2024-01-12 16:45:41 +0800

    finish plugin

--- src/plugin.py
+++ src/plugin.py
@@ -9,10 +9,35 @@ class UpsetGalgame(FlowLauncher):
         if rep.status_codes != 200:
             return [
                 {
-                    "Title": "Can not fetch search result",
-                    "SubTitle": f"Server return {rep.status_code}",
+                    "Title": "无法获取搜索结果",
+                    "subTitle": f"服务器返回： {rep.status_code}",
                 }
             ]
-            
+        results = rep.json()
+        if len(results) == 0:
+            return [
+                {
+                    
+                    "Title": "未搜索到相关游戏",
+                    "subTitle": "请尝试换个关键词进行搜索"
+                }
+            ]
+        query_result = []
+        for i in results:
+            name = i["name"]
+            size = i["size"]
+            query_result.append(
+                {
+                    "Title": name,
+                    "subTitle": f"文件大小: {size}",
+                    "jsonRPCAction": {
+                        "method": "open_url",
+                        "parameters": [f"https://www.shinnku.com/api/download{name}"]
+                    }
+                }
+            )
+        return query_result
+        
+
     def open_url(self, url):
         webbrowser.open(url)
