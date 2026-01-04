4356e3c3aa89939ee9c539e81eb57c9b4533dda7
luohua <156412791+iuohua@users.noreply.github.com>
2026-01-04 17:10:07 +0800

    fix import order

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
-import sys, os
-import os
 import sys
+import os
+
+from src.plugin import UpsetGalgame
 
 # add plugin to local PATH
 parent_folder_path = os.path.abspath(os.path.dirname(__file__))
 sys.path.append(parent_folder_path)
 sys.path.append(os.path.join(parent_folder_path, 'lib'))
 
-from src.plugin import UpsetGalgame
+
 
 if __name__ == "__main__":
     UpsetGalgame()
